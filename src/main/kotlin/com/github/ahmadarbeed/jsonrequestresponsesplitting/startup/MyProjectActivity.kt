a35610b0e6276437d42697d4340206c150d29607
GitHub Action <action@github.com>
2025-05-28 13:53:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/ahmadarbeed/jsonrequestresponsesplitting/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/ahmadarbeed/jsonrequestresponsesplitting/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.ahmadarbeed.jsonrequestresponsesplitting.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
